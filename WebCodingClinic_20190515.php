c85339083906fbf2851b7966e922822b6ba2e410
ParkSangBeom <46027250+ParkSangbeomm@users.noreply.github.com>
2019-05-15 22:02:47 +0900

    20190515_second

---
+++ WebCodingClinic_20190515.php
@@ -0,0 +1,128 @@
+<?php
+  $conn = mysqli_connect("localhost", "shb598999", "park990501", "shb598999");
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="euc-kr">
+    <head>
+<meta http-equiv="CONTENT-TYPE" content="text/html;charset=EUC-KR">
+<meta name="viewport" content="width=device-width, user-scalable=no">
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="description" content="토모나리,남자쇼핑몰,남성의류,트렌드,캐쥬얼,베이직,댄디,스트릿,트렌드,남자코디,커플룩">
+<meta name="keywords" content="베이직,남자쇼핑몰,남자쇼핑몰순위,악세사리,언더웨어,토모나리,남성의류,남자티셔츠,유니크스타일,남자옷,남자청바지,남자티셔츠,댄디스타일,남자코디,남자옷쇼핑몰,남자셔츠,슬렉스,바지,청바지,벨트,브이넥,라운드,신발,벨트,셔츠,티셔츠,팬츠,슬립온,캐쥬얼,유니크" >
+<meta name="author" content="tomonari siromuse">
+<meta name="copyright" content="siromuse">
+<meta name="reply-to" content="t_t0331@nate.com">
+<meta name="viewport" content="width=device-width">
+<meta name="naver-site-verification" content="fc295a6ecd84686421fc7de5f8a509e0ac6d13e3"/>
+<meta name="google-site-verification" content="L19Tm3iCP63kProqTi1E8qsg0oPUTH8be4G1hZEpyto" />
+<meta property="al:ios:url" content="tomonari.co.kr/app.html">
+<meta property="al:ios:app_store_id" content="tomonari">
+<meta property="al:ios:app_name" content="tomonari">
+<meta property="al:android:url" content="tomonari.co.kr/app.html">
+<meta property="al:android:app_name" content="tomonari">
+<meta property="al:android:package" content="tomonari">
+<meta property="al:web:url" content="http://tomonari.co.kr">
+<link rel="canonical" href="http://www.tomonari.co.kr">
+<link rel="shortcut icon" href="/shopimages/gujelove7/favicon.ico" type="image/x-icon">
+<link rel="apple-touch-icon-precomposed" href="/shopimages/gujelove7/mobile_web_icon.png" />
+<link type="text/css" rel="stylesheet" href="/images/d3/m_01/css/font-awesome.min.css" />
+
+<span itemscope="" itemtype="http://schema.org/Organization">
+<link itemprop="url" href="http://www.tomonari.co.kr">
+<a itemprop="sameAs" rel="me" href="https://play.google.com/store/apps/details?id=com.makeshop.powerapp.gujelove7"></a>
+<a itemprop="sameAs" rel="me" href="https://itunes.apple.com/app/id1009323260"></a>
+<a itemprop="sameAs" rel="me" href="https://www.facebook.com/tomoholic"></a>
+<a itemprop="sameAs" rel="me" href="https://instagram.com/tomonari.co.kr"></a>
+</span>
+
+
+<link rel="shortcut icon" href="/shopimages/gujelove7/favicon.ico" type="image/x-icon"><link rel="apple-touch-icon-precomposed" href="/shopimages/gujelove7/mobile_web_icon.png" /><link rel="image_src" href="http://www.tomonari.co.kr/shopimages/gujelove7/facebookimg.gif" />
+<meta property="og:title" content="토모나리" />
+<meta property="og:type" content="website" />
+<meta property="og:url" content="http://www.tomonari.co.kr/shop/shopbrand.html?xcode=103&type=X&gf_ref=Yz1yRklaWWs=" />
+<meta property="og:image" content="http://www.tomonari.co.kr/shopimages/gujelove7/facebookimg.gif" />
+<meta property="og:description" content="토모나리 가성비끝판왕, 남자쇼핑몰, 청바지/슬랙스, 트렌드/캐쥬얼, 당일발송, 신상품15%할인" /><title> 셔츠 | 토모나리</title>
+
+<link type="text/css" rel="stylesheet" href="/shopimages/gujelove7/template/work/3783/common.css" /><link href="//fonts.googleapis.com/css?family=Lato:100,300,400,500,700,900,100italic,300italic,400italic,700italic,900italic" rel="stylesheet" type="text/css">
+<link href="http://gujelove7.img2.kr/design2019/css/default.css" media="screen" rel="stylesheet" type="text/css" />
+<link href="http://gujelove7.img2.kr/design2019/css/style.css" media="screen" rel="stylesheet" type="text/css" />
+<link href="/design/gujelove7/js/swiper.min.css" media="screen" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="/design/gujelove7/js/swiper.min.js"></script>
+<script type="text/javascript" src="/design/gujelove7/js/jquery.min.js"></script>
+<script language='javascript' src='/design/gujelove7/js/default.js'></script>
+
+<!--  LOG corp Web Analitics & Live Chat  START -->
+<script  type="text/javascript">
+//<![CDATA[
+function logCorpAScript_full(){
+	HTTP_MSN_MEMBER_NAME="";/*member name*/
+	var prtc=(document.location.protocol=="https:")?"https://":"http://";
+	var hst=prtc+"asp7.http.or.kr";
+	var rnd="r"+(new  Date().getTime()*Math.random()*9);
+	this.ch=function(){
+		if(document.getElementsByTagName("head")[0]){logCorpAnalysis_full.dls();}else{window.setTimeout(logCorpAnalysis_full.ch,30)}
+	}
+	this.dls=function(){
+		var  h=document.getElementsByTagName("head")[0];
+		var  s=document.createElement("script");s.type="text/jav"+"ascript";try{s.defer=true;}catch(e){};try{s.async=true;}catch(e){};
+		if(h){s.src=hst+"/HTTP_MSN/UsrConfig/tomonari007/js/ASP_Conf.js?s="+rnd;h.appendChild(s);}
+	}
+	this.init= function(){
+		document.write('<img src="'+hst+'/sr.gif?d='+rnd+'"  style="width:1px;height:1px;position:absolute;" alt="" onload="logCorpAnalysis_full.ch()" />');
+	}
+}
+if(typeof logCorpAnalysis_full=="undefined"){	var logCorpAnalysis_full=new logCorpAScript_full();logCorpAnalysis_full.init();}
+//]]>
+</script>
+<noscript><img src="http://asp7.http.or.kr/HTTP_MSN/Messenger/Noscript.php?key=tomonari007" border="0" style="display:none;width:0;height:0;" /></noscript>
+<!-- LOG corp Web Analitics & Live Chat END  -->
+<base href="http://tomonari.co.kr" target="_blank">
+    <title></title>
+  </head>
+  <body>
+
+    <div class="wrap_g">
+    <div class="pro">
+    	 <table summary="상품 섬네일, 상품명, 가격">
+    		 <caption>best_product</caption>
+    		<colgroup>
+    			<col width="25%" />
+    			<col width="25%" />
+    			<col width="25%" />
+    			<col width="25%" />
+    		</colgroup>
+    		<tbody>
+    			 <tr>
+             <?php
+             $sql = "SELECT * FROM tomonari";
+             $result = mysqli_query($conn, $sql);
+             while($row = mysqli_fetch_array($result)) {
+             ?>
+               <td class="va w_20">
+      					 <div class="list_loll" style="cursor:pointer;">
+      					   <img class="MS_prod_img_s" src=<?= $row[2]?> >
+                    <div>
+                      <span class="pro_review"><span><?=$row['review']?></span></span>
+    					        <?= $row['title']?><span class="list_brand"> [ <?=$row['color']?> ,<?=$row['size']?> ]</span><br>
+  			              <span class="price "><?=$row['price']?></span>
+                      <span class="pro_text icon17"><span class='MK-product-icons'></span></span>
+
+                    </div>
+      					</div>
+      				</td>
+              <?php
+              }
+              ?>
+
+
+
+					</tr>
+    		</tbody>
+    	</table>
+    </div>
+    </div>
+  </body>
+</html>
