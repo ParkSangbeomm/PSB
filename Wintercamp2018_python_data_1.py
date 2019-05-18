31f73ac49944521af9a12a1d3f91aaf467c4d168
ParkSangBeom <46027250+ParkSangbeomm@users.noreply.github.com>
2019-05-19 02:15:56 +0900

    Update Wintercamp2018_python_data_1.py

--- Wintercamp2018_python_data_1.py
+++ Wintercamp2018_python_data_1.py
@@ -7,7 +7,7 @@
 #import re
 #from datetime import date, time, datetime, timedelta
 #from operator import itemgetter
-#import glob 
+#import glob er
 #  
 # #1: Print a simple string
 #print("Why not change the world?")
